2597d18382d456782ae51580012d97e372e959f8
Chris Willett <chris_willett77@yahoo.com>
2024-01-27 14:39:56 -0800

    Add whitenoise and middleware to enable static files with uvicorn

--- booking/routing.py
+++ booking/routing.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
-from .consumers import MyConsumer
+from .consumers import MyConsumer  # Ensure this is your correct consumer
 
 websocket_urlpatterns = [
     re_path(r'^ws/chat/$', MyConsumer.as_asgi()),
--- telehealth_app/settings.py
+++ telehealth_app/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'telehealth_app.urls'
